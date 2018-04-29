3169074e0a6b7bc726c13421aaf472d0ad3f68c0
Slava Knyazev <slava@knyz.org>
2018-04-29 16:08:18 -0400

    Completed Assignment

--- src/vknyazev_B20_A03_MusicSystem/MusicSystem.java
+++ src/vknyazev_B20_A03_MusicSystem/MusicSystem.java
@@ -4,27 +4,45 @@ package vknyazev_B20_A03_MusicSystem;
  * MusicSystem
  */
 import java.util.ArrayList;
-import java.io.*;
 import java.util.Scanner;
+import java.io.*;
 
 public class MusicSystem {
 
     private ArrayList<Song> songs = new ArrayList<Song>();
-    private Scanner sc;
-    private File sf;
+    private File sf = new File("music.txt");
 
-    public MusicSystem() throws IOException, FileNotFoundException{
-        sc = new Scanner(sf);
-        sc.useDelimiter("*");
+    public MusicSystem() throws IOException, FileNotFoundException {
+        Scanner sc = new Scanner(sf);
+        sc.useDelimiter("\\*|\n");
         while (sc.hasNext()) {
             int year = sc.nextInt();
+            // System.out.println(year);
             int ranking = sc.nextInt();
+            // System.out.println(ranking);
             String artist = sc.next();
+            // System.out.println(artist);
             String title = sc.next();
+            // System.out.println(title);
             int downloadCount = sc.nextInt();
+            // System.out.println(downloadCount);
             Song song = new Song(year, ranking, artist, title, downloadCount);
             songs.add(song);
-        }
+        } // while
+        sc.close();
+    } // MusicSystem()
+
+    public void writeSorted() throws IOException {
+        sortByRank();
+        sortByYear();
+
+        FileWriter fw = new FileWriter(new File("sortedMusic.txt"));
+
+        for (Song s : songs)
+            fw.write(s.getYear() + "*" + s.getRanking() + "*" + s.getArtist() + "*" + s.getTitle() + "*"
+                    + s.getDownloadCount() + "\n");
+
+        fw.close();
     }
 
     /**
@@ -32,20 +50,90 @@ public class MusicSystem {
      */
     public ArrayList<Song> getSongs() {
         return songs;
-    }
+    } // getSongs()
 
     /**
      * @param songs the songs to set
      */
     public void setSongs(ArrayList<Song> songs) {
         this.songs = songs;
-    }
+    } // setSongs()
 
+    /**
+     * 
+     */
     public void sortByYear() {
-        // TODO: add sorting
-    }
-    
+        int n = songs.size();
+        for (int j = 0; j < songs.size(); j++) {
+            for (int i = 1; i < (n - j); i++) {
+                Song song1 = songs.get(i);
+                Song song2 = songs.get(i - 1);
+                if (song1.getYear() < song2.getYear()) {
+                    songs.set(i, song2);
+                    songs.set(i - 1, song1);
+                }
+            }
+        }
+    } // sortByYear()
+
     public void sortByRank() {
-        // TODO: add sorting
+        int n = songs.size();
+        for (int j = 0; j < songs.size(); j++) {
+            for (int i = 1; i < (n - j); i++) {
+                Song song1 = songs.get(i);
+                Song song2 = songs.get(i - 1);
+                if (song1.getRanking() < song2.getRanking()) {
+                    songs.set(i, song2);
+                    songs.set(i - 1, song1);
+                }
+            }
+        }
+    } // sortByRank()
+
+    public void sortByTitle() {
+        int n = songs.size();
+        for (int j = 0; j < songs.size(); j++) {
+            for (int i = 1; i < (n - j); i++) {
+                Song song1 = songs.get(i);
+                Song song2 = songs.get(i - 1);
+                if (song1.getTitle().compareToIgnoreCase(song2.getTitle()) < 0) {
+                    songs.set(i, song2);
+                    songs.set(i - 1, song1);
+                }
+            }
+        }
+    } // sortByTitle()
+
+    public Song findSongByTitle(String title) {
+        // sort songs by title to enable a binary search
+        sortByTitle();
+
+        // Binary search
+        int bottom = 0;
+        int top = songs.size() - 1;
+        Song found = null;
+
+        while (bottom <= top && found == null) {
+
+            int middle = (bottom + top) / 2;
+
+            if (songs.get(middle).getTitle().equalsIgnoreCase(title))
+                found = songs.get(middle);
+            else if (songs.get(middle).getTitle().compareToIgnoreCase(title) < 0)
+                bottom = middle + 1;
+            else
+                top = middle - 1;
+
+        }
+
+        return found;
+    } // findSongByTitle()
+
+    public Song findSongByYearAndRanking(int year, int ranking) {
+        for (Song s : songs)
+            if (s.getRanking() == ranking && s.getYear() == year)
+                return s;
+
+        return null;
     }
 }
--- src/vknyazev_B20_A03_MusicSystem/MusicSystemFrame.java
+++ src/vknyazev_B20_A03_MusicSystem/MusicSystemFrame.java
@@ -1,26 +1,21 @@
 package vknyazev_B20_A03_MusicSystem;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.*;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JTextArea;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import javax.swing.text.*;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.*;
+import java.text.NumberFormat;
 
 public class MusicSystemFrame extends JFrame {
 
 	private JPanel contentPane;
 	private JTextArea textArea;
 	private MusicSystem ms;
-	
+
 	/**
 	 * Launch the application.
 	 */
@@ -43,13 +38,13 @@ public class MusicSystemFrame extends JFrame {
 	public MusicSystemFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 596, 383);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		
+
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
-		
+
 		JMenuItem mntmWrite = new JMenuItem("Write");
 		mntmWrite.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -57,7 +52,7 @@ public class MusicSystemFrame extends JFrame {
 			}
 		});
 		mnFile.add(mntmWrite);
-		
+
 		JMenuItem mntmExit = new JMenuItem("Exit");
 		mntmExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -65,10 +60,10 @@ public class MusicSystemFrame extends JFrame {
 			}
 		});
 		mnFile.add(mntmExit);
-		
+
 		JMenu mnSearch = new JMenu("Search");
 		menuBar.add(mnSearch);
-		
+
 		JMenuItem mntmBySongTitle = new JMenuItem("By song title");
 		mntmBySongTitle.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -76,18 +71,18 @@ public class MusicSystemFrame extends JFrame {
 			}
 		});
 		mnSearch.add(mntmBySongTitle);
-		
+
 		JMenuItem mntmByYearranking = new JMenuItem("By year/ranking");
 		mntmByYearranking.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				menu_search_year();
+				menu_search_year_ranking();
 			}
 		});
 		mnSearch.add(mntmByYearranking);
-		
+
 		JMenu mnReports = new JMenu("Reports");
 		menuBar.add(mnReports);
-		
+
 		JMenuItem mntmListAllMusic = new JMenuItem("List All Music");
 		mntmListAllMusic.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -95,7 +90,7 @@ public class MusicSystemFrame extends JFrame {
 			}
 		});
 		mnReports.add(mntmListAllMusic);
-		
+
 		JMenuItem mntmListByYear = new JMenuItem("List by year");
 		mntmListByYear.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -104,51 +99,161 @@ public class MusicSystemFrame extends JFrame {
 		});
 		mnReports.add(mntmListByYear);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
-		textArea = new JTextArea();
-		textArea.setEditable(false);
-		contentPane.add(textArea, BorderLayout.CENTER);
 
+		textArea = new JTextArea();
+		
+		textArea.setFont(new Font(Font.MONOSPACED, 0, 12));
+		textArea.setEditable(false);
+		JScrollPane sp = new JScrollPane(textArea);
+		contentPane.add(sp, BorderLayout.CENTER);
+		
 		try {
 			ms = new MusicSystem();
+		} catch (FileNotFoundException e) {
+			JOptionPane.showMessageDialog(this, "music.txt is missing", "Error", 0);
+			System.exit(1);
 		} catch (Exception e) {
+			e.printStackTrace();
 			JOptionPane.showMessageDialog(this, "Could not read data file", "Error", 0);
 			System.exit(1);
 		}
-		
+
+		if (ms.getSongs().size() == 0) {
+			JOptionPane.showMessageDialog(this, "Empty data file", "Error", 0);
+			System.exit(2);
+		}
 	}
-	
+
+	private String formatSongAsTableItemString(Song song) {
+		return String.format("%-10d%-10d%-30s%-30s%-10d \n", song.getYear(), song.getRanking(), song.getArtist(),
+				song.getTitle(), song.getDownloadCount());
+
+	}
+
+	private String makeTableHeader(String title) {
+		return String.format("%30s \n%30s \n\n%-10s%-10s%-30s%-30s%-10s \n%90s \n", title, title.replaceAll(".", "-"), "Year", "Ranking", "Artist", "Title",
+				"# Downloads",  new String(new char[90]).replace('\0', '-'));
+	}
+
 	private void menu_reports_year() {
+		String yearString = JOptionPane.showInputDialog(this, "Enter song name:");
+		if (yearString == null)
+			return;
+			
 		
+		try {
+			int year = Integer.parseInt(yearString);
+			ms.sortByRank();
+			ms.sortByYear();
+			textArea.setText("");
+
+			textArea.append(makeTableHeader("Top Songs of " + year));
+			for (Song song : ms.getSongs()) {
+				if (song.getYear() == year)
+					textArea.append(formatSongAsTableItemString(song));
+			}
+		} catch (NumberFormatException e) {
+			JOptionPane.showMessageDialog(this, "Year must be an integer", "Error", 0);
+		}
 	}
-	
+
 	private void menu_reports_all() {
-		
+		ms.sortByRank();
+		ms.sortByYear();
+		textArea.setText("");
+
+		textArea.append(makeTableHeader("Top Songs of the Year"));
+		NumberFormat nf = NumberFormat.getIntegerInstance();
+		int currentYear = ms.getSongs().get(0).getYear();
+		int downloadCount = 0;
+		for (Song song : ms.getSongs()) {
+			if (song.getYear() != currentYear) {
+				textArea.append(String.format("%85s\n", "-------"));
+				textArea.append(String.format("%68s: %15s \n\n", "Download Totals", nf.format(downloadCount * 1000)));
+
+				downloadCount = 0;
+				currentYear = song.getYear();
+			}
+			textArea.append(formatSongAsTableItemString(song));
+			downloadCount += song.getDownloadCount();
+		}
+
+		// print last year
+		textArea.append(String.format("%85s\n", "-------"));
+		textArea.append(String.format("%68s: %15s \n\n", "Download Totals", nf.format(downloadCount * 1000)));
+
+		textArea.setCaretPosition(0);
 	}
-	
-	private void menu_search_year(){
-		
+
+	private void menu_search_year_ranking() {
+		JPanel yearRankingSearchPanel = new JPanel(new GridLayout(2, 2));
+		JTextField fldYear = new JTextField();
+		JTextField fldRanking = new JTextField();
+		yearRankingSearchPanel.add(new JLabel("Year: "));
+		yearRankingSearchPanel.add(fldYear);
+		yearRankingSearchPanel.add(new JLabel("Ranking: "));
+		yearRankingSearchPanel.add(fldRanking);
+		int reply = JOptionPane.showConfirmDialog(this, yearRankingSearchPanel, "Search", JOptionPane.PLAIN_MESSAGE);
+		if (reply == 0) {
+			try {
+				int year = Integer.parseInt(fldYear.getText());
+				int ranking = Integer.parseInt(fldRanking.getText());
+				Song foundSong = ms.findSongByYearAndRanking(year,
+						ranking);
+				if (foundSong != null) {
+					textArea.setText(formatSongAsTableString(foundSong));
+				} else {
+					JOptionPane.showMessageDialog(this, "The year " + year + " and ranking " + ranking + " was not found.", "Error", 0);
+				}
+			} catch (NumberFormatException e) {
+				JOptionPane.showMessageDialog(this, "Fields must be integers", "Error", 0);
+			}
+
+		}
+
+		textArea.setCaretPosition(0);
 	}
-	
+
+	private String formatSongAsTableString(Song song) {
+		return String.format("%20s\n%20s\n%-25s %-25s\n%-25s %-25s\n%-25s %-25s\n%-25s %-25s\n%-25s %-25s\n", "Song Details", "-----------", "Year:", song.getYear(), "Ranking:", song.getRanking(), "Artist:", song.getArtist(), "Song Title:", song.getTitle(), "# Downloads:", song.getDownloadCount());
+	}
+
 	private void menu_search_title() {
 		String songTitle = JOptionPane.showInputDialog(this, "Enter song name:");
-		
+
 		// Kill if nothing entered
-		if (songTitle == null || songTitle == "")
+		if (songTitle == null)
 			return;
 
-		// TODO: Add search
+		Song foundSong = ms.findSongByTitle(songTitle);
+		if (foundSong == null) {
+			JOptionPane.showMessageDialog(this, "The song " + songTitle + " was not found.", "Error", 0);
+			return;
+		}
+		
+		textArea.setText(formatSongAsTableString(foundSong));
+
+
+		
 	}
-	
+
 	private void menu_file_exit() {
 		System.exit(0);
 	}
-	
+
 	private void menu_file_write() {
-		
+		try {
+			ms.writeSorted();
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(this, "Failed to write to disk.", "Error", 0);
+
+		}
+
 	}
 
+	private void listSongsByYear() {
+
+	}
 }
